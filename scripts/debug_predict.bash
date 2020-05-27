bb12365d46225a2bdd54ccd7cf5ff4ecd97fdd75
Aviad Moreshet <aviad10m@gmail.com>
2020-05-28 01:18:16 +0300

    Added a debug_predict script

---
+++ scripts/debug_predict.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+python -m debugpy --listen 5678 --wait-for-client object_detector_retinanet/keras_retinanet/bin/predict.py \
+  --base_dir "$DATASET_FOLDERNAME/images" \
+  --out "$DATASET_FOLDERNAME" \
+  --images-cls-cache "$FOLDERNAME/images_cls_cache" \
+    csv \
+  --annotations "$DATASET_FOLDERNAME/annotations/annotations_val_157_ann.csv" \
+  "$DATASET_FOLDERNAME/iou_resnet50_csv_06.h5"
