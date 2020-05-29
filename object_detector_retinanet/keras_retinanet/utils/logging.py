3cba8a32230b1a6ed67ecb7080edc939deef8c37
Aviad Moreshet <aviad10m@gmail.com>
2020-05-30 02:18:53 +0300

    Silencing tf logger

--- object_detector_retinanet/keras_retinanet/utils/logging.py
+++ object_detector_retinanet/keras_retinanet/utils/logging.py
@@ -23,6 +23,7 @@ class DummyTqdmFile(object):
 
 
 def configure_logging():
+    logging.getLogger('tensorflow').setLevel(logging.WARNING)
     logging.basicConfig(
         format='%(asctime)s: %(levelname).1s %(module)s.py %(funcName)s] %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S', level=logging.NOTSET,
