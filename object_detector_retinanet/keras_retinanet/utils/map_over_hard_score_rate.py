0c2fd5d49d61578e4d563023c913f62b2527b765
Aviad Moreshet <aviad10m@gmail.com>
2020-06-06 22:14:05 +0300

    Clear output before printing plot in map_over_hard_score_rate.py

--- object_detector_retinanet/keras_retinanet/utils/map_over_hard_score_rate.py
+++ object_detector_retinanet/keras_retinanet/utils/map_over_hard_score_rate.py
@@ -9,6 +9,7 @@ from object_detector_retinanet.keras_retinanet.utils.logger import configure_log
 from object_detector_retinanet.utils import create_dirpath_if_not_exist, rm_dir
 from bokeh.plotting import figure
 from bokeh.io import show, output_notebook
+from IPython.display import clear_output
 
 
 def assign_to_args(args, flag, val=None):
@@ -20,13 +21,15 @@ def assign_to_args(args, flag, val=None):
         if val is not None:
             args.insert(1, str(val))
 
+
 def vis_plot(hard_score_rates, average_precisions):
     fig = figure()
     fig.xaxis.axis_label = 'hard_score_rate'
     fig.yaxis.axis_label = 'AP@[ IoU=0.50:0.95 | area= all | maxDets=300 ]'
 
-    circle = fig.line(hard_score_rates, average_precisions, line_width=2)
+    fig.line(hard_score_rates, average_precisions, line_width=2)
     show(fig)
+
 
 if __name__ == '__main__':
     configure_logging()
@@ -41,7 +44,8 @@ if __name__ == '__main__':
     # Inject temp_folder to all temp created files, so we can delete it in the end
     temp_folder_path = './temp'
     create_dirpath_if_not_exist(temp_folder_path)
-    assign_to_args(args, '--save-path', os.path.join(temp_folder_path, 'save-folder'))
+    assign_to_args(args, '--save-path',
+                   os.path.join(temp_folder_path, 'save-folder'))
     assign_to_args(args, '--out', temp_folder_path)
     assign_to_args(args, '--predict-from-cache')
     hard_score_rates = np.linspace(0, 1, 11)
@@ -52,7 +56,6 @@ if __name__ == '__main__':
         assign_to_args(args, '--hard_score_rate', hard_score_rate)
         ap_all, _, _, _ = predict(args)
         average_precisions.append(ap_all)
+    clear_output()
     vis_plot(hard_score_rates, average_precisions)
     rm_dir(temp_folder_path)
-
-    
