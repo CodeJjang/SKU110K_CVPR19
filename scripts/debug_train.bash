2440dee3d607c8b30d213adc1fedd9be1f321f90
Aviad Moreshet <aviad10m@gmail.com>
2020-06-21 17:17:34 +0300

    Added debug_train script

---
+++ scripts/debug_train.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+python -m debugpy --listen 5678 --wait-for-client object_detector_retinanet/keras_retinanet/bin/train.py \
+  --snapshot-path "$FOLDERNAME/snapshots" \
+  --tensorboard-dir "$FOLDERNAME/logs" \
+  --images-cls-cache "$FOLDERNAME/images_cls_cache" \
+  --epochs 1 \
+  --steps 1 \
+  --max-annotations 157 \
+    csv \
+  --base_dir "$DATASET_FOLDERNAME/images" \
+  --annotations "$DATASET_FOLDERNAME/annotations/annotations_train.csv" \
+  --val-annotations "$DATASET_FOLDERNAME/annotations/annotations_val.csv"
