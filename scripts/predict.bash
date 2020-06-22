e2af5a42628b6d246e26f5017fea674c6f640a86
Aviad Moreshet <aviad10m@gmail.com>
2020-06-23 01:46:15 +0300

    Added pseudo_labeling script and removed debug_predict.bash

--- scripts/predict.bash
+++ scripts/predict.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
-python -u object_detector_retinanet/keras_retinanet/bin/predict.py \
+FLAGS="-u"
+if [ "$DEBUG" = "true" ]; then
+  FLAGS="-m debugpy --listen 5678 --wait-for-client"
+fi
+python $FLAGS object_detector_retinanet/keras_retinanet/bin/predict.py \
   --base_dir "$DATASET_FOLDERNAME/images" \
   --out "$DATASET_FOLDERNAME" \
   --images-cls-cache "$FOLDERNAME/images_cls_cache" \
