e2e4c8774ee452c6034e835a0d90569b9eeac417
Aviad Moreshet <aviad10m@gmail.com>
2020-06-23 01:46:29 +0300

    Added nms pseudo_labeling, seems to be working

--- object_detector_retinanet/keras_retinanet/utils/pseudo_labeling.py
+++ object_detector_retinanet/keras_retinanet/utils/pseudo_labeling.py
@@ -6,6 +6,7 @@ import csv
 import datetime
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 from object_detector_retinanet.keras_retinanet.bin.predict import main as predict
 from object_detector_retinanet.keras_retinanet.utils.logger import configure_logging
 from object_detector_retinanet.keras_retinanet.utils.to_coco import load_annotations_to_df
@@ -13,31 +14,34 @@ from object_detector_retinanet.keras_retinanet.utils import EmMerger
 
 
 class PseudoLabeling:
-    def __init__(self, gt_path, dt_path, hard_score_rate):
-        gt_df = load_annotations_to_df(args.annotations, 'ground-truths')
+    def __init__(self, gt_path, dt_path, hard_score_rate, out_dir):
+        gt_df = load_annotations_to_df(gt_path, 'ground-truths')
         gt_df['confidence'] = 1
         gt_df['hard_score'] = 1
 
-        dt_df = load_annotations_to_df(args.annotations, 'detections')
+        dt_df = load_annotations_to_df(dt_path, 'detections')
 
         self.gt_df = gt_df
         self.dt_df = dt_df
         self.hard_score_rate = hard_score_rate
         self.out_dir = out_dir
+        self.nms_iou_threshold = 0.5
+        self.max_detections = 9999
 
     def em_merger(self):
         timestamp = datetime.datetime.utcnow()
         res_file = os.path.join(self.out_dir, 'detections_output_iou_{}_{}.csv'.format(
             self.hard_score_rate, timestamp))
-        image_names_gt = list(set(gt_df['image_name']))
-        csv_data_lst = []
-        csv_data_lst.append(['image_id', 'x1', 'y1', 'x2',
-                             'y2', 'confidence', 'hard_score'])
+        image_names_gt = list(set(self.gt_df['image_name']))
+        csv_rows = []
         for image_name in image_names_gt:
-            height = gt_df['height']
-            width = gt_df['width']
+            height = self.gt_df.loc[self.gt_df['image_name']
+                                    == image_name]['image_height'].values[0]
+            width = self.gt_df.loc[self.gt_df['image_name']
+                                   == image_name]['image_width'].values[0]
 
-            df = pd.concat([gt_df[image_name], dt_df[image_name]])
+            df = pd.concat([self.gt_df.loc[self.gt_df['image_name'] == image_name],
+                            self.dt_df.loc[self.dt_df['image_name'] == image_name]])
 
             image_boxes = df[['x1', 'y2', 'x2', 'y2']].to_numpy()
             image_scores = df['confidence'].to_numpy()
@@ -45,21 +49,57 @@ class PseudoLabeling:
             image_labels = np.zeros((len(df.index), 1))
             results = np.concatenate(
                 [image_boxes, np.expand_dims(image_scores, axis=1), np.expand_dims(image_hard_scores, axis=1),
-                 np.expand_dims(image_labels, axis=1)], axis=1)
+                 image_labels], axis=1)
 
             filtered_data = EmMerger.merge_detections(
                 '', image_name, (height, width, 3), results, self.hard_score_rate)
 
             for ind, detection in filtered_data.iterrows():
                 row = [image_name, detection['x1'], detection['y1'], detection['x2'], detection['y2'],
-                       detection['confidence'], detection['hard_score']]
-                csv_data_lst.append(row)
+                       'object', height, width]
+                csv_rows.append(row)
 
+        self.to_csv(res_file, csv_rows)
+
+    def nms(self):
+        timestamp = datetime.datetime.utcnow()
+        res_file = os.path.join(self.out_dir, 'detections_output_iou_{}_{}.csv'.format(
+            self.hard_score_rate, timestamp))
+        image_names_gt = list(set(self.gt_df['image_name']))
+        csv_rows = []
+        for image_name in image_names_gt:
+            height = self.gt_df.loc[self.gt_df['image_name']
+                                    == image_name]['image_height'].values[0]
+            width = self.gt_df.loc[self.gt_df['image_name']
+                                   == image_name]['image_width'].values[0]
+
+            df = pd.concat([self.gt_df.loc[self.gt_df['image_name'] == image_name],
+                            self.dt_df.loc[self.dt_df['image_name'] == image_name]])
+
+            image_boxes = df[['x1', 'y2', 'x2', 'y2']].to_numpy()
+            image_scores = df['confidence'].to_numpy()
+            image_hard_scores = df['hard_score'].to_numpy()
+            scores = self.hard_score_rate * image_hard_scores + (1 - self.hard_score_rate) * image_scores
+
+            indices = tf.image.non_max_suppression(
+                image_boxes, scores, self.max_detections, iou_threshold=self.nms_iou_threshold,
+                score_threshold=float('-inf'), name=None
+            )
+            detections = tf.Session().run(image_boxes[indices])
+
+            for detection in detections:
+                row = [image_name, detection['x1'], detection['y1'], detection['x2'], detection['y2'],
+                       'object', height, width]
+                csv_rows.append(row)
+
+        self.to_csv(res_file, csv_rows)
+
+    def to_csv(self, fname, rows):
         # Save annotations csv file
-        with open(res_file, 'w') as fl_csv:
+        with open(fname, 'w') as fl_csv:
             writer = csv.writer(fl_csv)
-            writer.writerows(csv_data_lst)
-        logging.info(f'Saved output file at: {res_file}')
+            writer.writerows(rows)
+        logging.info(f'Saved output file at: {fname}')
 
 
 def parse_args(args):
@@ -90,3 +130,5 @@ if __name__ == '__main__':
         args.annotations, args.predicted_annotations, args.hard_score_rate, args.out)
     if args.dup_removal_tactic == 'em-merger':
         pl.em_merger()
+    elif args.dup_removal_tactic == 'nms':
+        pl.nms()
